5fa56c4d29ca348165bfec11e9320a6ba849fdd6
石岡将明 <ishioka0528@icloud.com>
2018-02-05 18:03:23 +0900

    いらないコードを消した

--- src/components/pages/Signin.vue
+++ src/components/pages/Signin.vue
@@ -5,7 +5,6 @@
 <script>
 import queryString from 'query-string'
 import axios from 'axios'
-import config from '../../../config'
 
 export default {
   data () {
@@ -19,7 +18,7 @@ export default {
       alert('missing')
     } else {
       console.log(this.code)
-      axios.post(`https://qiita.com/api/v2/access_tokens?client_id=8f6a2128c7be42bbec9331f4c463c19095bb4a81&client_secret=${config}&code=${this.code}`)
+      axios.post(`https://qiita.com/api/v2/access_tokens?client_id=8f6a2128c7be42bbec9331f4c463c19095bb4a81&client_secret=22e992bac4de1edccbd399c351af955c1635caaf&code=${this.code}`)
         .then(res => {
           console.log(res)
           localStorage.setItem('token', res.data.token)
